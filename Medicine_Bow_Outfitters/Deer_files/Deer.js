84dae6f37f79cb426dad6bfa0e4741dfd7d3163f
James Nennemann <jnennemann@gmail.com>
2016-11-02 20:21:28 -0500

    Temp web site host for MBO
    Just temp, as Dropbox dropped html rendering

---
+++ Medicine_Bow_Outfitters/Deer_files/Deer.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20161102
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,135),url:'Deer_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Deer_files/stroke_1.png'},{rect:new IWRect(1,-1,180,2),url:'Deer_files/stroke_2.png'},{rect:new IWRect(181,-1,2,2),url:'Deer_files/stroke_3.png'},{rect:new IWRect(181,1,2,135),url:'Deer_files/stroke_4.png'},{rect:new IWRect(181,136,2,2),url:'Deer_files/stroke_5.png'},{rect:new IWRect(1,136,180,2),url:'Deer_files/stroke_6.png'},{rect:new IWRect(-1,136,2,2),url:'Deer_files/stroke_7.png'}],new IWSize(182,137))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Deer_files/DeerMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixAllIEPNGs('Media/transparent.gif');Widget.onload();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Medicine_Bow_Outfitters/Elk_files/Elk.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20161102
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-3,3,6,105),url:'Elk_files/stroke.png'},{rect:new IWRect(-3,-2,6,5),url:'Elk_files/stroke_1.png'},{rect:new IWRect(3,-2,139,5),url:'Elk_files/stroke_2.png'},{rect:new IWRect(142,-2,6,5),url:'Elk_files/stroke_3.png'},{rect:new IWRect(142,3,7,105),url:'Elk_files/stroke_4.png'},{rect:new IWRect(142,108,6,4),url:'Elk_files/stroke_5.png'},{rect:new IWRect(3,108,139,5),url:'Elk_files/stroke_6.png'},{rect:new IWRect(-3,108,6,5),url:'Elk_files/stroke_7.png'}],new IWSize(146,110)),stroke_1:new IWStrokeParts([{rect:new IWRect(-1,1,2,147),url:'Elk_files/stroke_8.png'},{rect:new IWRect(-1,-1,2,2),url:'Elk_files/stroke_9.png'},{rect:new IWRect(1,-1,164,2),url:'Elk_files/stroke_10.png'},{rect:new IWRect(165,-1,1,2),url:'Elk_files/stroke_11.png'},{rect:new IWRect(165,1,1,147),url:'Elk_files/stroke_12.png'},{rect:new IWRect(165,148,1,2),url:'Elk_files/stroke_13.png'},{rect:new IWRect(1,148,164,2),url:'Elk_files/stroke_14.png'},{rect:new IWRect(-1,148,2,2),url:'Elk_files/stroke_15.png'}],new IWSize(165,149))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Elk_files/ElkMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixAllIEPNGs('Media/transparent.gif');Widget.onload();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
