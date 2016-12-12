1e45484505d60260fca73543fd895e2c4f8dfe6a
James Nennemann <jnennemann@gmail.com>
2016-12-12 13:17:29 -0600

    changes12122

--- Medicine_Bow_Outfitters/Hunts_files/Hunts.js
+++ Medicine_Bow_Outfitters/Hunts_files/Hunts.js
@@ -4,6 +4,6 @@ setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Hunts_files/HuntsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();IMpreload('Hunts_files','shapeimage_1','0');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');Widget.onload();IMpreload('Hunts_files','shapeimage_1','0');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
